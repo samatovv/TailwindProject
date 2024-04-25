25943eb7141380ccbd75037050b4cdd81e3ed9a5
Bekjan <temenovbekzan@gmail.com>
2024-04-25 17:02:32 +0600

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-
 import './App.css'
 
+import Header from "./Components/Header/Header"
 function App() {
 
   return (
     <>
       <h1 className="text-3xl font-bold underline">
-        Hello world!
+        <Header/>
       </h1>
     </>
   )
