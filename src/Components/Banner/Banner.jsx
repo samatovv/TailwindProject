26adcb7ac91d928fc31e6d7adced079ec13d7781
Bekjan <temenovbekzan@gmail.com>
2024-04-29 11:01:24 +0600

    banner

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -3,14 +3,17 @@ import 'react-html5video/dist/styles.css';
 
 import { Container } from "../helpers/Container"
 import video from "../../assets/Videos/video.mp4"
+import posterImage from "../../assets/IMGS/png/video.png";
+
+
 function Banner() {
   return (
     <Container>
-      <Video autoPlay loop
-      >
+      <Video autoPlay loop poster={posterImage}>
         <source src={video} type="video/webm" />
       </Video>
     </Container>
   );
 }
+
 export default Banner;
