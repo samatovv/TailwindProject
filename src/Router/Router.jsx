dbaac80fe668245e03d584b814997b44bf493f5f
Abdurakhman <abdurahmansamatovic@gmail.com>
2024-04-29 12:02:25 +0600

    bedroom

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -29,7 +29,7 @@ export const router = createBrowserRouter([{
         },
         {
             path: "/model",
-            element: <ModelPage />
+            element: <ModelPage/>
         },
         {
             path: "/onas",
